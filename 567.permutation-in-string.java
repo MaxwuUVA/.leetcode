990b6fbd466d8ef2724bd53d2e287b25f0032af0
MaxwuUVA <31987705+MaxwuUVA@users.noreply.github.com>
2019-09-04 23:21:33 -0400

    Create 567.permutation-in-string.java

---
+++ 567.permutation-in-string.java
@@ -0,0 +1,46 @@
+import java.util.HashSet;
+import java.util.Set;
+
+/*
+ * @lc app=leetcode id=567 lang=java
+ *
+ * [567] Permutation in String
+ */
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+
+        if(s1.length() > s2.length()) return false;
+
+        int[] map1 = new int[26];
+        int[] map2 = new int[26];
+
+        for(int i = 0;i<s1.length();i++){
+            map1[s1.charAt(i)-'a']++;
+            map2[s2.charAt(i)-'a']++;
+        }
+        for(int i = 0;i < s2.length()-s1.length();i++){
+            if(ismatch(map1, map2)){
+                return true;
+            }
+            map2[s2.charAt(i)-'a']--;
+            map2[s2.charAt(i+s1.length())-'a']++;
+
+        }
+
+        return ismatch(map1, map2);
+    }
+    private boolean ismatch(int[] map1,int[] map2){
+
+        for(int i = 0;i < 26;i++){
+            if(map1[i] != map2[i]){
+
+                return false;
+
+            }
+        }
+        return true;
+
+    }
+
+}
+
