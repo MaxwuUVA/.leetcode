378293ce5a5bd1775ce9ecf77068459233108045
MaxwuUVA <31987705+MaxwuUVA@users.noreply.github.com>
2019-09-06 23:29:01 -0400

    Create 1080.insufficient-nodes-in-root-to-leaf-paths.java

---
+++ 1080.insufficient-nodes-in-root-to-leaf-paths.java
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode id=1080 lang=java
+ *
+ * [1080] Insufficient Nodes in Root to Leaf Paths
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode sufficientSubset(TreeNode root, int limit) {
+
+        if(root == null) return root;
+        if(root.left == null && root.right == null) 
+          return root.val < limit ? null:root;
+        root.left = sufficientSubset(root.left, limit-root.val);
+        root.right = sufficientSubset(root.right, limit-root.val);
+        if(root.left == null && root.right == null)
+          return null;
+        return root;
+    }
+}
+
