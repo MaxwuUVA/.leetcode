d0081a480f404e1ebfe48448a966384dbabf946e
MaxwuUVA <31987705+MaxwuUVA@users.noreply.github.com>
2019-09-12 21:38:45 -0400

    Create 14.longest-common-prefix.java

--- 14.longest-common-prefix.java
+++ 14.longest-common-prefix.java
@@ -11,6 +11,12 @@ class Solution {
         boolean find  = false;
 
         StringBuilder sb = new StringBuilder();
+        //暴力法
+        //以第一个string为model
+        //遍历string array中所有的元素
+        //从第0个元素开始，找是否所有元素在相同位置有相同的char
+        //O(n*l) l为元素有几个char
+
         for(int i = 0;i < strs[0].length();i++){
             char cur = strs[0].charAt(i);
             for(int j = 1;j<strs.length;j++){
