820b71f8307c38b63948316f53b3d905876948e2
MaxwuUVA <31987705+MaxwuUVA@users.noreply.github.com>
2019-09-22 00:46:20 -0400

    Create 300.longest-increasing-subsequence.java

---
+++ 300.longest-increasing-subsequence.java
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode id=300 lang=java
+ *
+ * [300] Longest Increasing Subsequence
+ */
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+            if(nums.length == 0) return 0;
+            //LIS问题
+            //O(n^2)
+            int len = nums.length,max = 1;
+            int[] dp = new int[nums.length];
+            Arrays.fill(dp,1);
+            for(int i = 0;i < len;i++){
+                for(int j = 0;j < i;j++){
+                    if(nums[i] > nums[j] && dp[j]+1 > dp[i]){
+                        dp[i] = dp[j]+1;
+                    }
+                }
+    
+                max = Math.max(max,dp[i]);
+                
+            }
+            
+            return max;
+            
+    }
+}
+
