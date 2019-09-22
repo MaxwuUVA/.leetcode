5c9188a46b57a546497e21570e426685859a81ae
MaxwuUVA <31987705+MaxwuUVA@users.noreply.github.com>
2019-09-22 00:46:37 -0400

    Update 376.wiggle-subsequence.java

--- 376.wiggle-subsequence.java
+++ 376.wiggle-subsequence.java
@@ -9,13 +9,34 @@ class Solution {
         if(nums.length == 0) return 0;
         
        //greedy
-       int up = 1, down = 1;
+       /*int up = 1, down = 1;
        for(int i = 1;i < nums.length; i++){
            if(nums[i] > nums[i-1]) up = down + 1;
            if(nums[i] < nums[i-1]) down = up + 1;
        }        
-       return Math.max(up,down);
+       return Math.max(up,down);*/
        //todo 更新 动态规划方法
+       int[] up = new int[nums.length+1];
+       int[] down = new int[nums.length+1];
+       up[1] = 1;
+       down[1] = 1;
+       for(int i = 1;i < nums.length;i++){
+           if(nums[i] > nums[i-1]){
+               up[i+1] = down[i]+1;
+               down[i+1] = down[i];
+           }
+           else if(nums[i-1] > nums[i]){
+               down[i+1] = up[i]+1;
+               up[i+1] = up[i];
+           }
+           else{
+              down[i+1] = down[i];
+              up[i+1] = up[i];
+           }
+       }
+
+       return Math.max(up[nums.length], down[nums.length]);
+
     }
 }
 
