a7973ccef0668cf45819d5444f3fbf7f65015522
MaxwuUVA <31987705+MaxwuUVA@users.noreply.github.com>
2019-09-22 00:46:51 -0400

    Create UnionFind.java

---
+++ UnionFind.java
@@ -0,0 +1,34 @@
+public class UnionFind{
+    private int[] parent,rank;
+    //private int count = 0;
+    public  UnionFind(int n){
+        //count = n;
+        parent = new int[n];
+        rank = new int[n];
+        for(int i = 0;i < n;i++){
+            parent[i] = i;
+        }
+    }
+    public int find(int p){
+        while (p != parent[p]) {
+            parent[p] = parent[parent[p]];    // path compression by halving
+            p = parent[p];
+        }
+        return p;
+    }
+    public void Union(int p,int q){
+        int rootP = find(p);
+        int rootQ = find(q);
+        if (rootP == rootQ) return;
+        if (rank[rootQ] > rank[rootP]) {
+            parent[rootP] = rootQ;
+        }
+        else {
+            parent[rootQ] = rootP;
+        if (rank[rootP] == rank[rootQ]) {
+              rank[rootP]++;
+          }
+        }
+             //count--;
+    }
+}
