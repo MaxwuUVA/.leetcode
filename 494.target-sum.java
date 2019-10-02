cf36735eab19dbdee21338669c5b2c611ec72ec6
MaxwuUVA <31987705+MaxwuUVA@users.noreply.github.com>
2019-10-02 00:24:24 -0400

    Create 494.target-sum.java

---
+++ 494.target-sum.java
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode id=494 lang=java
+ *
+ * [494] Target Sum
+ */
+class Solution {
+    public int findTargetSumWays(int[] nums, int S) {
+
+        //array
+        //dp
+        
+    }
+}
+
